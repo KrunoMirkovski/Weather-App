60246b56aa039d9b78fb2d15076a2f48dce0e578
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-12-04 01:50:07 +0100

    Creating project folder structure

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,22 @@
+import "./Weather.module.css"
+import search_icon from "./Assets/icons8-search-50.png";
+import humidity_icon from "./Assets/icons8-humidity-50.png";
+import sunny_icon from "./Assets/icons8-summer-48.png";
+import cloud_icon from "./Assets/icons8-cloud-48.png";
+import rain_icon from "./Assets/icons8-rain-48.png";
+import snow_icon from "./Assets/icons8-snow-48.png";
+import thunder_icon from "./Assets/icons8-storm-48.png";
+import wind_icon from "./Assets/icons8-wind-48.png";
+
+
+
+
+const WeatherApp = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default WeatherApp
